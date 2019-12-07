03367934fc159911625c691b481c62b44914baaf
Bussman <gingflow15@gmail.com>
2019-12-06 21:53:08 -0800

    Alex

--- tiktok.js
+++ tiktok.js
@@ -8,10 +8,10 @@ var y=10;
 var hy = 0;
 var T = 0;
 for(i=0; i<3; i++){
-for (x=0; x<2; x++) {
+for (x=0; x<3; x++) {
     ctx.moveTo(x*23.3+23.3+T*80, y);
     ctx.lineTo(x*23.3+23.3+T*80, y+70);
-//jordan is a normal sized bitch
+
     ctx.moveTo(hy+T*80, x*23.3+33.3);
     ctx.lineTo(hy+70+T*80, x*23.3+33.3);
     ctx.stroke(); 
