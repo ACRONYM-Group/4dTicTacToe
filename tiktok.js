2df876dce447627d2ed13f42bba41f80a8e1e11c
Bussman <gingflow15@gmail.com>
2019-12-06 22:30:21 -0800

    Made the grids scale infinitely

--- tiktok.js
+++ tiktok.js
@@ -6,18 +6,19 @@ c.height = window.innerHeight*.95;
 var ctx = c.getContext("2d");
 var drawx = 40;
 var drawy = 40;
-
+var Lmax = 8;
 
 var y=10;
 var hy = 0;
 var T = 0;
 for(i=0; i<3; i++){
-for (x=0; x<3; x++) {
-    ctx.moveTo(x*23.3+23.3+T*80, y);
-    ctx.lineTo(x*23.3+23.3+T*80, y+70);
-
-    ctx.moveTo(hy+T*80, x*23.3+33.3);
-    ctx.lineTo(hy+70+T*80, x*23.3+33.3);
+for (x=0; x<Lmax; x++) {
+    //vert line draw
+    ctx.moveTo(x*23.3+23.3+T*40*Lmax, y);
+    ctx.lineTo(x*23.3+23.3+T*40*Lmax, y+23.3*Lmax+23.3);
+// horizontal line draw
+    ctx.moveTo(hy+T*40*Lmax, x*23.3+33.3);
+    ctx.lineTo(hy+23.3*Lmax+23.3+T*40*Lmax, x*23.3+33.3);
     ctx.stroke(); 
 }
 T++
