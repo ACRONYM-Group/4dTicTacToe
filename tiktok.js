63219d47e740ed341ac56bf937402cd405b62f1c
Bussman <gingflow15@gmail.com>
2019-12-06 21:21:28 -0800

    Drawboard

---
+++ tiktok.js
@@ -0,0 +1,20 @@
+var c = document.getElementById("TicTacToe");
+var ctx = c.getContext("2d");
+var drawx = 40;
+var drawy = 40;
+
+
+var y=10;
+var hy = 0;
+var T = 0;
+for(i=0; i<3; i++){
+for (x=0; x<2; x++) {
+    ctx.moveTo(x*23.3+23.3+T*80, y);
+    ctx.lineTo(x*23.3+23.3+T*80, y+70);
+
+    ctx.moveTo(hy+T*80, x*23.3+33.3);
+    ctx.lineTo(hy+70+T*80, x*23.3+33.3);
+    ctx.stroke(); 
+}
+T++
+}
