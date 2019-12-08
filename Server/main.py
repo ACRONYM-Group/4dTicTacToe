2fba634474691c582bc8d413d465242bc368bea3
jordanayan <jordanayan@live.com>
2019-12-07 18:51:17 -0600

    Win Condition Checking

--- Server/main.py
+++ Server/main.py
@@ -50,6 +50,54 @@ def setCell(coords, val, i=0):
 
     exec(str(subArrayString) + " = val")
 
+def checkVictory():
+    bx = 0
+    while bx < width:
+        by = 0
+        while by < width:
+            sx = 0
+            while sx < width:
+                sy = 0
+                while sy < width:
+                    returnData = checkIfInCenterOfLine(bx, by, sx, sy)
+                    foundWinCondition = returnData[0]
+                    winner = returnData[1]
+                    sy += 1
+                sx += 1
+
+            by += 1
+        bx += 1 
+    
+    return [foundWinCondition, winner]
+
+
+def checkIfInCenterOfLine(bx, by, sx, sy):
+    foundWinCondition = False
+    adjacentCells = []
+    focusbx = -1
+    while focusbx > -2 and focusbx < 2:
+        focusby = -1
+        while focusby > -2 and focusby < 2:
+            focussx = -1
+            while focussx > -2 and focussy < 2:
+                focussy = -1
+                while focussy > -2 and focussy < 2:
+                    if (boardState[bx+focusbx][by+focusby][sx+focussx][sy+focussy] == boardState[bx][by][sx][sy]):
+                        adjacentCells.append([focusbx, focusby, focussx, focussy])
+                    focussy += 1
+
+                focussx += 1
+            focusby +=1
+        focusbx += 
+        
+    for i in adjacentCells:
+        for k in adjacentCells:
+            if i[0] == -k[0] and i[1] == -k[1] and i[2] == -k[2] and i[3] == -k[3]:
+                foundWinCondition = True
+                winner = boardState[i[0]][i[1]][i[2]][i[3]]
+
+    return [foundWinCondition, winner]
+
 async def commandHandler(msg, websocket):
     print(msg["cmdtype"])
 
@@ -60,6 +108,9 @@ async def commandHandler(msg, websocket):
     if msg["cmdtype"] == "setCell":
         print(msg["coords"])
         setCell(msg["coords"], msg["val"])
+        victory = checkVictory()
+        if victory[0]:
+            print(victory[1] + " HAS WON THE MATCH!")
 
     if (msg["cmdtype"] == "getCell"):
         await websocket.send(json.dumps({"cmdtype":"getCellResponse", "coords":msg["coords"], "val":getCell(msg["coords"])}))
